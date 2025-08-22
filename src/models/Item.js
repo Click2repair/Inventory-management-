ee55e4a6dc26c07021bfb9b598abdf8fb605be92
Click2repair <amarjitsah670@gmail.com>
2025-08-22 15:26:53 +0530

    Update Item.js

--- src/models/Item.js
+++ src/models/Item.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    price: { type: Number, required: true, min: 0 },
+    quantity: { type: Number, required: true, min: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Item', itemSchema);
 
