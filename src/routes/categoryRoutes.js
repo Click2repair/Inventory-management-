58ef0ab9956e58dffd6446f56484b8dc86513a7b
Click2repair <amarjitsah670@gmail.com>
2025-08-22 15:29:57 +0530

    Update categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/categoryController');
+
+router.post('/', ctrl.createCategory);
+router.get('/', ctrl.getCategories);
+router.get('/:id', ctrl.getCategoryById);
+router.put('/:id', ctrl.updateCategory);
+router.delete('/:id', ctrl.deleteCategory);
+
+module.exports = router;
 
