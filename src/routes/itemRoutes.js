763b3a3eecaf4aaf179e5a83b521fc6f8112558c
Click2repair <amarjitsah670@gmail.com>
2025-08-22 15:30:29 +0530

    Update itemRoutes.js

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/itemController');
+
+router.post('/', ctrl.createItem);
+router.get('/', ctrl.getItems);
+router.get('/:id', ctrl.getItemById);
+router.put('/:id', ctrl.updateItem);
+router.delete('/:id', ctrl.deleteItem);
+
+// List items by category
+router.get('/by-category/:categoryId', ctrl.getItemsByCategory);
+
+module.exports = router;
 
